2c432b7741a4923299a0b974bda63e558ea8039f
yomea <951645267@qq.com>
2017-04-25 09:22:02 +0800

    add comment
    add comment

--- Rhino/src/com/rhino/TestRhino.java
+++ Rhino/src/com/rhino/TestRhino.java
@@ -1,4 +1,4 @@
-package com.rhino;
+package rhino;
 
 import java.io.FileReader;
 import java.net.URL;
@@ -7,6 +7,17 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
+/**
+ * class jdk.nashorn.api.scripting.NashornScriptEngine
+ * youth
+ * world
+ * class jdk.nashorn.api.scripting.ScriptObjectMirror
+ * 3.0
+ * 计算后的值为：30
+
+ * @author Administrator
+ *
+ */
 public class TestRhino {
 	
 	public static void main(String[] args) throws Exception {
@@ -16,17 +27,24 @@ public class TestRhino {
 		ScriptEngine engine = sem.getEngineByName("JavaScript");
 		//或者ScriptEngine engine = sem.getEngineByName("js");
 		
-		System.out.println(engine.getClass());
-		
-		engine.put("msg", "hello");
-		
-		engine.eval("var a={name:'youth'}; print(a.name);");
-		//脚本定义的变量会存在engine的上下文中
-		engine.eval("msg = 'world';");
+		System.out.println(engine.getClass());// class jdk.nashorn.api.scripting.NashornScriptEngine
+		//这个对象中持有一个继承了Map的Binding类，这个类用来储存键值对
+		engine.put("msg", "hello");//hello
 		
 		System.out.println(engine.get("msg"));
 		
-		System.out.println(engine.get("a").getClass());
+		//这个方法类似与javascript中的eval方法
+		engine.eval("var a={name:'youth'}; print(a.name);");//youth
+		//脚本定义的变量会存在engine的上下文中,执行赋值运算后，msg的值发生了改变
+		engine.eval("msg = 'world';");
+		
+		engine.eval("test = 'neihou';");
+		
+		System.out.println(engine.get("test"));// world
+		
+		System.out.println(engine.get("msg"));// world
+		
+		System.out.println(engine.get("a").getClass());//class jdk.nashorn.api.scripting.ScriptObjectMirror
 		
 		engine.eval("function add(a, b) {return a + b;}");
 		
@@ -34,7 +52,7 @@ public class TestRhino {
 		
 		Object result = invoke.invokeFunction("add", 1, 2);
 		
-		System.out.println(result);
+		System.out.println(result);//3.0
 		
 		//String jsCode = "importPackage(java.util);var list = Arrays.asList(\"a\", \"b\",\"c\");";
 		
@@ -43,8 +61,8 @@ public class TestRhino {
 		URL url = TestRhino.class.getClassLoader().getResource("a.js");
 		
 		FileReader reader = new FileReader(url.getPath());
-		
-		engine.eval(reader);
+		//运行一个js文件
+		engine.eval(reader);//计算后的值为：30
 		
 	}
 
